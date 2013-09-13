54ff07a28e705756b5e6da29e14c0a70a80c99bf
kimkha <nkimkha@gmail.com>
2013-09-14 00:26:58 +0700

    Add category.

---
+++ vi_VN/categories.php
@@ -0,0 +1,28 @@
+<?php
+
+return array(
+
+	'category' => 'Thể loại',
+	'categories' => 'Các thể loại',
+
+	'create_category' => 'Tạo thể loại mới',
+	'edit_category' => 'Sửa &ldquo;%s&rdquo;',
+
+	// form fields
+	'title' => 'Tiêu đề',
+	'title_explain' => 'Tiêu đề thể loại của bạn.',
+	'title_missing' => 'Vui lòng nhập một tiêu đề',
+
+	'slug' => 'Chuỗi slug',
+	'slug_explain' => 'Chuỗi slug cho thể loại của bạn.',
+
+	'description' => 'Mô tả',
+	'description_explain' => 'Thể loại của bạn nói về cái gì.',
+
+	// messages
+	'created' => 'Thể loại mới của bạn đã được tạo.',
+	'updated' => 'Thể loại của bạn đã được cập nhật.',
+	'deleted' => 'Thể loại của bạn đã bị xoá.',
+	'delete_error' => 'Bạn phải có ít nhất một thể loại.',
+
+);
