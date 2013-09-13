89c40f2d1604ff702807ec0e9c6efb2c33082651
kimkha <nkimkha@gmail.com>
2013-09-14 06:30:50 +0700

    Add comments.

---
+++ vi_VN/comments.php
@@ -0,0 +1,35 @@
+<?php
+
+return array(
+
+	'comments' => 'Bình Luận',
+	'nocomments_desc' => 'Chưa có bình luận nào.',
+	'editing_comment' => 'Sửa Đổi Bình Luận',
+	'view_comment' => 'Xem Bình Luận',
+
+	// form fields
+	'name' => 'Tên',
+	'name_explain' => 'Tên tác giả',
+	'name_missing' => 'Vui lòng nhập tên',
+
+	'email' => 'Địa chỉ email',
+	'email_explain' => 'Email tác giả',
+	'email_missing', 'Vui lòng nhập một email hợp lệ', // frontend message (appears on your site!)
+
+	'text' => 'Bình luận',
+	'text_explain' => '',
+	'text_missing' => 'Vui lòng nhập nội dung bình luận', // frontend message (appears on your site!)
+
+	'status' => 'Trạng thái',
+	'status_explain' => '',
+
+	// messages
+	'created' => 'Bình luận của bạn đã được thêm', // frontend message (appears on your site!)
+	'updated' => 'Bình luận của bạn đã được cập nhật',
+	'deleted' => 'Bình luận của bạn đã bị xoá',
+
+	// email notification
+	'notify_subject' => 'Bình luận mới vừa được thêm',
+	'nofity_heading' => 'Một bình luận mới vừa được gửi lên trang web của bạn.'
+
+);
