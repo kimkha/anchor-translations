c7d5f6c8a54e5652dc50023ab7d721538576af0c
kimkha <nkimkha@gmail.com>
2013-09-14 09:26:43 +0700

    Add global messages

---
+++ vi_VN/global.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * Global is to replace common.php
+ *
+ * This global file will provide the majority of the translations in anchor
+ * to remove duplicate translations in section specific language files.
+ */
+return array(
+
+	// words
+	'save' => 'Lưu',
+	'delete' => 'Xoá',
+	'update' => 'Cập nhật',
+	'edit' => 'Sửa',
+	'editing' => 'Đang sửa',
+	'create' => 'Tạo',
+	'created' => 'Đã tạo',
+	'submit' => 'Gửi',
+	'close' => 'Đóng',
+	'status' => 'Trạng thái',
+	'manage' => 'Quản lý',
+	'reset' => 'Trở lại ban đầu',
+	'all' => 'Tất cả',
+
+	// pagination
+	'next' => 'Tiếp',
+	'previous' => 'Trước',
+	'first' => 'Đầu',
+	'last' => 'Cuối',
+
+	// statuses
+	'draft' => 'Nháp',
+	'archived' => 'Đã lưu',
+	'published' => 'Đã công bố',
+	'pending' => 'Đang chờ',
+	'approved' => 'Được chấp nhận',
+	'spam' => 'Spam',
+
+	'inactive' => 'Tắt',
+	'active' => 'Bật',
+
+	// roles
+	'administrator' => 'Quản trị',
+	'editor' => 'Biên tập',
+	'user' => 'Người dùng',
+
+	'log_in' => 'Đăng nhập',
+	'login' => 'Đăng nhập',
+	'log_out' => 'Đăng xuất',
+	'logout' => 'Đăng xuất',
+
+	// pharses
+	'visit_your_site' => 'Lượt xem trang',
+	'powered_by_anchor' => 'Cung cấp bởi Anchor, phiên bản %s',
+	'make_blogging_beautiful' => 'Giúp viết blog tuyệt vời hơn.',
+
+	// messages
+	'confirm_delete' => 'Bạn có chắc chắc muốn xoá? Thao tác này không thể phục hồi!'
+
+);
