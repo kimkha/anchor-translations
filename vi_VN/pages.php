ee5369e8c04b714d5a6d07c5f4be7ac50e764537
kimkha <nkimkha@gmail.com>
2013-09-14 09:41:44 +0700

    Add pages

---
+++ vi_VN/pages.php
@@ -0,0 +1,48 @@
+<?php
+
+return array(
+
+	'pages' => 'Trang',
+
+	'create_page' => 'Tạo một trang mới',
+	'nopages_desc' => 'Bạn không có bất cứ trang nào.',
+	'redirect' => 'Đổi hướng',
+
+	// form fields
+	'redirect_url' => 'Url đổi hướng',
+	'redirect_missing' => 'Vui lòng nhập một url hợp lệ',
+
+	'title' => 'Tựa đề trang',
+	'title_explain' => '',
+	'title_missing' => 'Vui lòng nhập một tựa đề trang',
+
+	'content' => 'Nội dung',
+	'content_explain' => 'Nội dung trang của bạn. Sử dụng Markdown.',
+
+	'show_in_menu' => 'Hiển Thị Trong Trình Đơn',
+	'show_in_menu_explain' => '',
+
+	'name' => 'Tên',
+	'name_explain' => '',
+
+	'slug' => 'Chuỗi Slug',
+	'slug_explain' => 'Chuỗi slug để nhận diện trang của bạn, chỉ nên chứa ký tự mã ascii',
+	'slug_missing' => 'Vui lòng nhập chuỗi slug, chuỗi slug chỉ có thể chứa ký tự mã ascii',
+	'slug_duplicate' => 'Chuỗi slug đã tồn tại',
+	'slug_invalid' => 'Chuỗi slug phải chứa chữ',
+
+	'status' => 'Trạng Thái',
+	'status_explain' => '',
+
+	'parent' => 'Trang Cha',
+	'parent_explain' => '',
+
+	// messages
+	'updated' => 'Trang của bạn đã được cập nhật.',
+	'created' => 'Trang của bạn đã được tạo.',
+	'deleted' => 'Trang của bạn đã bị xoá.',
+
+	'cannot_delete_home_page' => 'Bạn không thể xoá trang chủ',
+	'cannot_delete_posts_page' => 'Bạn không thể xoá trang liệt kê bài viết',
+
+);
