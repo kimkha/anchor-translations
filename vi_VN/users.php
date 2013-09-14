8df3296c91f7f6ebb7e2a2096ad03d7c362c1028
kimkha <nkimkha@gmail.com>
2013-09-14 09:58:22 +0700

    Add users

---
+++ vi_VN/users.php
@@ -0,0 +1,67 @@
+<?php
+
+return array(
+
+	'users' => 'Người Dùng',
+
+	'create_user' => 'Tạo một người dùng mới',
+	'add_user' => 'Thêm một người dùng mới',
+	'editing_user' => 'Sửa Trang Cá Nhân của %s',
+	'remembered' => 'Lưu mật khẩu',
+	'forgotten_password' => 'Quên mật khẩu?',
+
+	// roles
+	'administrator' => 'Quản Trị',
+	'administrator_explain' => '',
+
+	'editor' => 'Biên Tập',
+	'editor_explain' => '',
+
+	'user' => 'Người Dùng',
+	'user_explain' => '',
+
+	// form fields
+	'real_name' => 'Tên Thật',
+	'real_name_explain' => '',
+
+	'bio' => 'Tiểu Sử',
+	'bio_explain' => '',
+
+	'status' => 'Trạng Thái',
+	'status_explain' => '',
+
+	'role' => 'Vai Trò',
+	'role_explain' => '',
+
+	'username' => 'Tên Đăng Nhập',
+	'username_explain' => '',
+	'username_missing' => 'Vui lòng nhập tên đăng nhập, phải có ít nhất %s ký tự',
+
+	'password' => 'Mật Khẩu',
+	'password_explain' => '',
+	'password_too_short' => 'Mật khẩu phải có ít nhất %s ký tự',
+
+	'new_password' => 'Mật Khẩu Mới',
+
+	'email' => 'Email',
+	'email_explain' => '',
+	'email_missing' => 'Vui lòng nhập một địa chỉ email hợp lệ',
+	'email_not_found' => 'Không tìm thấy trang cá nhân.',
+
+	// messages
+	'updated' => 'Trang cá nhân đã cập nhật.',
+	'created' => 'Trang cá nhân đã được tạo.',
+	'deleted' => 'Trang cá nhân đã bị xoá.',
+	'delete_error' => 'Bạn không thể xoá trang cá nhân của chính mình',
+	'login_error' => 'Tên đăng nhập hoặc mật khẩu không đúng.',
+	'logout_notice' => 'Bạn đã đăng xuất.',
+	'recovery_sent' => 'Chúng tôi đã gửi bạn một email để xác nhận việc thay đổi mật khẩu.',
+	'recovery_expired' => 'Mã phục hồi mật khẩu đã hết hạn, hãy thử lại lần nữa.',
+	'password_reset' => 'Mật khẩu mới của bạn đã được lưu. Đăng nhập liền bây giờ đi!',
+
+	// password recovery email
+	'recovery_subject' => 'Phục Hồi Mật Khẩu',
+	'recovery_message' => 'Bạn đã yêu cầu phục hồi lại mật khẩu của bạn.' .
+		'Để tiếp tục hãy nhấp vào liên kết bên dưới.' . PHP_EOL . '%s',
+
+);
