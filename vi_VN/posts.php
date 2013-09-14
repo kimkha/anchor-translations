6107efa4503301d1431b7ae4f47f98eab717fdf6
kimkha <nkimkha@gmail.com>
2013-09-14 09:48:58 +0700

    Add posts

---
+++ vi_VN/posts.php
@@ -0,0 +1,47 @@
+<?php
+
+return array(
+
+	'posts' => 'Bài Viết',
+
+	'create_post' => 'Tạo bài viết mới',
+	'noposts_desc' => 'Bạn không có bài viết nào cả!',
+
+	// form fields
+	'title' => 'Tựa đề bài viết',
+	'title_explain' => '',
+	'title_missing' => 'Vui lòng nhập tựa đề',
+
+	'content' => 'Nội dung bài viết',
+	'content_explain' => 'Viết thôi.',
+
+	'slug' => 'Chuỗi slug',
+	'slug_explain' => 'Chuỗi slug để nhận diện bài viết của bạn, chỉ nên chứa ký tự mã ascii',
+	'slug_missing' => 'Vui lòng nhập chuỗi slug, chuỗi slug chỉ có thể chứa ký tự mã ascii',
+	'slug_duplicate' => 'Chuỗi slug đã tồn tại',
+	'slug_invalid' => 'Chuỗi slug phải chứa chữ',
+
+	'description' => 'Mô tả',
+	'description_explain' => '',
+
+	'status' => 'Trạng Thái',
+	'status_explain' => '',
+
+	'category' => 'Thể Loại',
+	'category_explain' => '',
+
+	'allow_comments' => 'Cho Phép Bình Luận',
+	'allow_comments_explain' => '',
+
+	'custom_css' => 'CSS Tuỳ Chỉnh',
+	'custom_css_explain' => '',
+
+	'custom_js' => 'JS Tuỳ Chỉnh',
+	'custom_js_explain' => '',
+
+	// messages
+	'updated' => 'Bài viết của bạn đã được cập nhật.',
+	'created' => 'Bài viết của bạn đã được tạo.',
+	'deleted' => 'Bài viết của bạn đã bị xoá.',
+
+);
