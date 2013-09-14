734738809751669f2b1ae73c0bfebb8002d19c7c
kimkha <nkimkha@gmail.com>
2013-09-14 09:43:23 +0700

    Add plugins

---
+++ vi_VN/plugins.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+
+	'plugins' => 'Trình Cắm',
+	'plugins_desc' => 'Quản lý trình cắm của bạn',
+	'noplugins_desc' => 'Viết chương trình thôi',
+
+	// messages
+	'installed' => '%s đã được cài đặt',
+	'uninstalled' => '%s đã bị gỡ bỏ'
+);
