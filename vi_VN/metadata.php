1f056b4238cc34425e3c276437b60933832debf0
kimkha <nkimkha@gmail.com>
2013-09-14 09:34:11 +0700

    Add metadata

---
+++ vi_VN/metadata.php
@@ -0,0 +1,45 @@
+<?php
+
+return array(
+
+	'metadata' => 'Thông Tin Cơ Bản',
+	'metadata_desc' => 'Quản lý thông tin trang web của bạn',
+
+	'comment_settings' => 'Bình luận',
+	'theme_settings' => 'Giao diện',
+
+	// form fields
+	'sitename' => 'Tên trang web',
+	'sitename_explain' => '',
+	'sitename_missing' => 'Trang web của bạn cần một cái tên!',
+
+	'sitedescription' => 'Mô tả trang web',
+	'sitedescription_explain' => '',
+	'sitedescription_missing' => 'Trang web của bạn cần một mô tả!',
+
+	'homepage' => 'Trang Chủ',
+	'homepage_explain' => '',
+
+	'postspage' => 'Trang Bài Viết',
+	'postspage_explain' => '',
+
+	'posts_per_page' => 'Bài viết trong trang',
+	'posts_per_page_explain' => 'Số lượng bài viết trong một trang',
+
+	'auto_publish_comments' => 'Tự động chấp nhận bình luận',
+	'auto_publish_comments_explain' => '',
+
+	'comment_notifications' => 'Thông báo email cho mỗi bình luận mới',
+	'comment_notifications_explain' => '',
+
+	'comment_moderation_keys' => 'Từ khoá spam',
+	'comment_moderation_keys_explain' => 'Danh sách phân cách bởi dấu phẩy các từ khoá được đưa vào danh sách đen.
+		Từ đó, các bình luận sẽ được tự động đánh dấu spam.',
+
+	'current_theme' => 'Giao diện hiện tại',
+	'current_theme_explain' => '',
+
+	// messages
+	'updated' => 'Thông tin cơ bản đã cập nhật',
+
+);
